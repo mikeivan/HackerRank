64bd7cb6494e17852a82438b52fafa2a6e1bc453
mikeivan <mikeivan@users.noreply.github.com>
2017-06-07 11:00:18 -0700

    Create Solution.java

---
+++ Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,22 @@
+/************
+ * Author: Michael "Mike" Ivan
+ * HackerRank profile: 
+ *    https://www.hackerrank.com/mike_ivan
+ * GitHub: https://github.com/mikeivan
+ ************/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
