eb18ddc3b0c3b937b180eb0c5c7b989a8bf7afa7
mikeivan <mikeivan@users.noreply.github.com>
2017-06-07 10:56:34 -0700

    Create Solution.java

---
+++ Java/Introduction/Welcome to Java/Solution.java
@@ -0,0 +1,15 @@
+/************
+ * Author: Michael "Mike" Ivan
+ * HackerRank profile: 
+ *    https://www.hackerrank.com/mike_ivan
+ * GitHub: https://github.com/mikeivan
+ ************/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
