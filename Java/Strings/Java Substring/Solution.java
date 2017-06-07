346cbcba3defcbeb583911a4cf131445de539a8a
mikeivan <mikeivan@users.noreply.github.com>
2017-06-07 12:09:29 -0700

    Create Solution.java

---
+++ Java/Strings/Java Substring/Solution.java
@@ -0,0 +1,24 @@
+/************
+ * Author: Michael "Mike" Ivan
+ * HackerRank profile: 
+ *    https://www.hackerrank.com/mike_ivan
+ * GitHub: https://github.com/mikeivan
+ ************/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        System.out.println(S.substring(start, end));
+    }
+}
