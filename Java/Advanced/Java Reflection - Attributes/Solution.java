b951a76c3a9ff133284529ea3fe2dcfdcee8573d
mikeivan <mikeivan@users.noreply.github.com>
2017-06-12 12:09:14 -0700

    Create Solution.java

---
+++ Java/Advanced/Java Reflection - Attributes/Solution.java
@@ -0,0 +1,25 @@
+/************
+ * Author: Michael "Mike" Ivan
+ * Date: 6/5/17
+ * HackerRank profile: 
+ *    https://www.hackerrank.com/mike_ivan
+ * GitHub: https://github.com/mikeivan
+ ************/
+ 
+public class Solution {
+
+        public static void main(String[] args){
+            Class student = Student.class;
+            Method[] methods = student.getDeclaredMethods();
+
+            ArrayList<String> methodList = new ArrayList<>();
+            for(Method m : methods){
+                methodList.add(m.getName());
+            }
+            Collections.sort(methodList);
+            for(String name: methodList){
+                System.out.println(name);
+            }
+        }
+
+    }
