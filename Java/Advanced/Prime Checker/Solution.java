4aafe6a589e912f0a8efc2569ec89203d002a08e
mikeivan <mikeivan@users.noreply.github.com>
2017-06-12 14:56:52 -0700

    Create Solution.java

---
+++ Java/Advanced/Prime Checker/Solution.java
@@ -0,0 +1,87 @@
+/************
+ * Author: Michael "Mike" Ivan
+ * Date: 6/5/17
+ * HackerRank profile: 
+ *    https://www.hackerrank.com/mike_ivan
+ * GitHub: https://github.com/mikeivan
+ ************/
+ 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+import static java.lang.System.in;
+
+class Prime {
+        
+    void checkPrime(int ... list) {
+        boolean isPrime = false;
+        
+        for(int num : list) {
+            isPrime = true;
+            
+            //check if prime, if yes print
+            if( num <= 1 ) {
+                isPrime = false;
+                continue;
+            }
+            
+            for(int i = 2; i < num; i++) {
+                if( num%i == 0 ) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if( isPrime == true ) {
+                System.out.print(num + " ");
+            }
+        }
+        
+        System.out.println();
+    }
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		try{
+		BufferedReader br=new BufferedReader(new InputStreamReader(in));
+		int n1=Integer.parseInt(br.readLine());
+		int n2=Integer.parseInt(br.readLine());
+		int n3=Integer.parseInt(br.readLine());
+		int n4=Integer.parseInt(br.readLine());
+		int n5=Integer.parseInt(br.readLine());
+		Prime ob=new Prime();
+		ob.checkPrime(n1);
+		ob.checkPrime(n1,n2);
+		ob.checkPrime(n1,n2,n3);
+		ob.checkPrime(n1,n2,n3,n4,n5);	
+		Method[] methods=Prime.class.getDeclaredMethods();
+		Set<String> set=new HashSet<>();
+		boolean overload=false;
+		for(int i=0;i<methods.length;i++)
+		{
+			if(set.contains(methods[i].getName()))
+			{
+				overload=true;
+				break;
+			}
+			set.add(methods[i].getName());
+			
+		}
+		if(overload)
+		{
+			throw new Exception("Overloading not allowed");
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+}
